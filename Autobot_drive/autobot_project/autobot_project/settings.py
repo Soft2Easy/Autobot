f8c89c6b97879700bf0dd560452b397793acca89
Katlego <ralehlakakatlego@gmail.com>
2024-07-06 01:34:48 +0200

    changes

--- Autobot_drive/autobot_project/autobot_project/settings.py
+++ Autobot_drive/autobot_project/autobot_project/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'autobot_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autobot_app'
+
+
 ]
 
 MIDDLEWARE = [
