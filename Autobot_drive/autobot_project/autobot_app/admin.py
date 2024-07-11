537a3976b6b66cc4d40e06953ce8dd924a486af4
Katlego <ralehlakakatlego@gmail.com>
2024-07-12 01:06:43 +0200

    basic models added

--- Autobot_drive/autobot_project/autobot_app/admin.py
+++ Autobot_drive/autobot_project/autobot_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, ServiceRec, ServicePro
 
 # Register your models here.
 
 admin.site.register(Car)
+admin.site.register(ServiceRec)
+admin.site.register(ServicePro)
--- Autobot_drive/autobot_project/autobot_app/models.py
+++ Autobot_drive/autobot_project/autobot_app/models.py
@@ -11,3 +11,24 @@ class Car(models.Model):
 
     def __str__(self):
         return f"{self.vin},{self.make},{self.model},{self.year}"
+
+
+class ServicePro(models.Model):
+    name = models.CharField(max_length=100)
+    cellphone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.name},{self.cellphone},{self.email}"
+
+
+class ServiceRec(models.Model):
+    date = models.DateTimeField()
+    type_of_service = models.CharField(max_length=100)
+    service_provider = models.ForeignKey(ServicePro, related_name='service_provider', on_delete=models.CASCADE)
+    note = models.CharField(max_length=200)
+    car = models.ForeignKey(Car, related_name='service_record', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date},{self.type_of_service},{self.service_provider},{self.note}"
+
